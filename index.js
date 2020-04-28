b772a25eceb27d522f01ad1fefc132b18000322e
Jacob Gantt <gantt42@gmail.com>
2020-04-27 21:00:54 -0400

    Add dev tools window.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { app, BrowserWindow } = require('electron')
 
+var devtools
+
 function createWindow () {
   // Create the browser window.
   let win = new BrowserWindow({
@@ -11,8 +13,22 @@ function createWindow () {
   })
 
   win.setMenu(null)
+
+  win.on('closed', () => {
+    if(!devtools.isDestroyed()){
+      devtools.close()
+    }
+  })
+
+  devtools = new BrowserWindow({show: false})
+  devtools.setBounds({ x: 0, y: 0,})
+  devtools.minimize()
+
   // and load the index.html of the app.
   win.loadFile('index.html')
+
+  win.webContents.setDevToolsWebContents(devtools.webContents)
+  win.webContents.openDevTools()
 }
 
 app.whenReady().then(createWindow)
