3cef00fa27d27272e85adf79d80e16f8f4efc636
Jacob Gantt <gantt42@gmail.com>
2020-05-01 23:55:05 -0400

    Add server-page

---
+++ server-page/server-page.js
@@ -0,0 +1,34 @@
+$(function(){
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var data = JSON.parse(this.responseText);
+
+      function addDataToTbody(creatureGrid, data) {
+        data.slice(0, 10).forEach((rowData, rowDataIndex) => {
+
+          var nameImageTemplate = document.querySelector('#mytemplate')
+
+          var nameImagePlacement = document.createElement("div")
+
+          nameImagePlacement.innerHTML = nameImageTemplate.innerHTML
+
+          // set
+          nameImagePlacement.querySelector('#creatureImage').src =
+            'https://lemurware.tech/api/v1/creatures/' + rowData.moniker +'/image'
+          nameImagePlacement.querySelector('#creatureName').textContent =
+            rowData.name
+
+          creatureGrid.appendChild(nameImagePlacement)
+        })
+      }
+
+      var creatureGrid = document.querySelector("#creatureGrid")
+
+      addDataToTbody(creatureGrid, data);
+
+    }
+  };
+  xhttp.open("GET", "https://lemurware.tech/api/v1/creatures/", true);
+  xhttp.send();
+});
