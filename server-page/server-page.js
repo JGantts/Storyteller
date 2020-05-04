e393704fd63f0bfc9a6685979780c5207cc2c22a
Jacob Gantt <gantt42@gmail.com>
2020-05-04 01:55:57 -0400

    Switch to fetch.

--- server-page/server-page.js
+++ server-page/server-page.js
@@ -1,36 +1,37 @@
-$(function(){
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      var data = JSON.parse(this.responseText);
+async function asyncCall() {
+  let creaturesResponse = await fetch('https://lemurware.tech/api/v1/creatures/')
+  if (creaturesResponse.ok){
+    let creaturesData = await creaturesResponse.json();
+    let creatureGrid = document.querySelector("#creatureGrid")
+    creaturesData.slice(0, 10).forEach(async (rowData, rowDataIndex) => {
 
-      function addDataToTbody(creatureGrid, data) {
-        data.slice(0, 10).forEach((rowData, rowDataIndex) => {
+      var nameImageTemplate = document.querySelector('#mytemplate')
 
-          var nameImageTemplate = document.querySelector('#mytemplate')
+      var nameImagePlacement = document.importNode(nameImageTemplate.content, true);
 
-          var nameImagePlacement = document.importNode(nameImageTemplate.content, true);
+      // set
+      nameImagePlacement.querySelector('#creatureImage').src =
+        'https://lemurware.tech/api/v1/creatures/' + rowData.moniker +'/image'
 
-          // set
-          nameImagePlacement.querySelector('#creatureImage').src =
-            'https://lemurware.tech/api/v1/creatures/' + rowData.moniker +'/image'
-          nameImagePlacement.querySelector('#creatureName').textContent =
-            rowData.name
-          nameImagePlacement.querySelector('#creatureLink').addEventListener("click", function(){
-            window.creatureMoniker = rowData.moniker;
-            $("#main").load("creature-page/creature-page.html");
-          });
-
-          creatureGrid.appendChild(nameImagePlacement)
-        })
+      let nameResponse = await fetch('https://lemurware.tech/api/v1/creatures/' + rowData.moniker +'/name')
+      if (nameResponse.ok){
+        let nameData = await nameResponse.json();
+        nameImagePlacement.querySelector('#creatureName').textContent = nameData.name;
+      }else {
+        alert("HTTP-Error: " + response.status);
       }
 
-      var creatureGrid = document.querySelector("#creatureGrid")
+      nameImagePlacement.querySelector('#creatureLink').addEventListener("click", function(){
+        window.creatureMoniker = rowData.moniker;
+        $("#main").load("creature-page/creature-page.html");
+      });
 
-      addDataToTbody(creatureGrid, data);
+      creatureGrid.appendChild(nameImagePlacement)
+    })
 
-    }
-  };
-  xhttp.open("GET", "https://lemurware.tech/api/v1/creatures/", true);
-  xhttp.send();
-});
+    nameImagePlacement.querySelector('#creatureName').textContent = nameData.name;
+  }else{
+    alert("HTTP-Error: " + creaturesResponse.status);
+  }
+}
+asyncCall()
