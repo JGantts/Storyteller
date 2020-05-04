098cd94c5537e600ebab2947af299db784386bd2
Jacob Gantt <gantt42@gmail.com>
2020-05-04 15:10:18 -0400

    Add settings page.

--- menu-bar-left.js
+++ menu-bar-left.js
@@ -1,5 +1,5 @@
-function loadServerPage(clicked){
-  $("#main").load("server-page/server-page.html");
+function loadSettingsPage(clicked){
+  $("#main").load("settings-page/settings-page.html");
   var current = document.getElementsByClassName("active");
   current[0].className = current[0].className.replace(' active', '');
   document.getElementById(clicked).className += ' active';
@@ -7,6 +7,13 @@ function loadServerPage(clicked){
 
 function loadLocalPage(clicked){
   $("#main").load("local-page/local-page.html");
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(' active', '');
+  document.getElementById(clicked).className += ' active';
+}
+
+function loadServerPage(clicked){
+  $("#main").load("server-page/server-page.html");
   var current = document.getElementsByClassName("active");
   current[0].className = current[0].className.replace(' active', '');
   document.getElementById(clicked).className += ' active';
