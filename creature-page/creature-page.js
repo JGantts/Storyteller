84f41f61016068479dbc63075c854e5650abee7f
Jacob Gantt <gantt42@gmail.com>
2020-05-04 03:14:53 -0400

    Start of creature-page kin section.

--- creature-page/creature-page.js
+++ creature-page/creature-page.js
@@ -1,13 +1,56 @@
-document.querySelector('#creatureMoniker').innerHTML = window.creatureMoniker;
+$(async function(){
+  document.querySelector('#creatureMoniker').innerHTML = window.creatureMoniker;
 
-document.querySelector('#creatureImage').src = 'https://lemurware.tech/api/v1/creatures/' + window.creatureMoniker +'/image'
+  document.querySelector('#creatureImage').src = 'https://lemurware.tech/api/v1/creatures/' + window.creatureMoniker + '/image';
 
-var xhttpName = new XMLHttpRequest();
-xhttpName.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    var nameData = JSON.parse(this.responseText);
-    document.querySelector('#creatureName').innerHTML = nameData.name;
+  let kinResponse = await fetch('https://lemurware.tech/api/v1/creatures/' + window.creatureMoniker + '/kin');
+  if (kinResponse.ok){
+    let kinData = await kinResponse.json();
+
+    switch (kinData.conceptionEventType) {
+      case 0:
+        let conceptionEventTemplate = document.querySelector('#kin-parents-conceived')
+        let conceptionEventPlacement = document.importNode(conceptionEventTemplate.content, true);
+
+        conceptionEventPlacement.querySelector('#parent1Link').addEventListener("click", function(){
+          window.creatureMoniker = kinData.parent1Moniker;
+          $("#main").load("creature-page/creature-page.html");
+        });
+
+        conceptionEventPlacement.querySelector('#parent2Link').addEventListener("click", function(){
+          window.creatureMoniker = kinData.parent2Moniker;
+          $("#main").load("creature-page/creature-page.html");
+        });
+
+        (async function(parentNamePlacement){
+          let nameResponse = await fetch('https://lemurware.tech/api/v1/creatures/' + kinData.parent1Moniker +'/name')
+          if (nameResponse.ok){
+            let nameData = await nameResponse.json();
+            parentNamePlacement.textContent = nameData.name;
+          }else{
+            alert("HTTP-Error: " + response.status);
+          }
+        }(conceptionEventPlacement.querySelector('#parent1Name')));
+
+        (async function(parentNamePlacement){
+          let nameResponse = await fetch('https://lemurware.tech/api/v1/creatures/' + kinData.parent2Moniker +'/name')
+          if (nameResponse.ok){
+            let nameData = await nameResponse.json();
+            parentNamePlacement.textContent = nameData.name;
+          }else{
+            alert("HTTP-Error: " + response.status);
+          }
+        }(conceptionEventPlacement.querySelector('#parent2Name')));
+
+        document.querySelector("#creatureKinParent").appendChild(conceptionEventPlacement);
+
+        break;
+
+      default:
+
+    }
+
+  }else{
+    alert("HTTP-Error: " + response.status);
   }
-};
-xhttpName.open('GET', 'https://lemurware.tech/api/v1/creatures/' + window.creatureMoniker + '/name', true);
-xhttpName.send();
+});
