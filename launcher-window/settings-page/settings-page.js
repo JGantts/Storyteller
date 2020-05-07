ace60ef659124627fa0daa580d8c9d9bcce2bb44
Jacob Gantt <gantt42@gmail.com>
2020-05-06 23:41:12 -0400

    Add setting statehint.

--- launcher-window/settings-page/settings-page.js
+++ launcher-window/settings-page/settings-page.js
@@ -2,5 +2,16 @@ $('.setting-checkbox').each(function() {
   this.checked = settings.get(this.getAttribute('data-backend-name'), this.getAttribute('data-default'))
   this.addEventListener('change', (event) => {
     settings.set(event.target.getAttribute('data-backend-name'), event.target.checked);
+    setStatehint(event.target);
   });
+  setStatehint(this);
  });
+
+function setStatehint(checkbox){
+  let stateHintDivSelector = '#' + checkbox.id + '-statehint';
+  $(stateHintDivSelector).text('(' + (
+    checkbox.checked
+    ? $(stateHintDivSelector).attr('data-checked-statehint')
+    : $(stateHintDivSelector).attr('data-unchecked-statehint')
+  ) + ')');
+}
