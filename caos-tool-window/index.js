adf8493d36cab28c5fe2f6d3cb4c878bae67e569
Jacob Gantt <gantt42@gmail.com>
2020-05-07 01:50:50 -0400

    Beginnings of a parser.
    Simply removes all whitespace/comments and chunks the code.

--- caos-tool-window/index.js
+++ caos-tool-window/index.js
@@ -7,3 +7,32 @@ function injectUserCode(){
       document.getElementById('caos-result').innerHTML = result;
   });
 }
+
+function parseText(){
+  var code = $('#caos-user-code').val();
+
+  chunks = chunkCode(code);
+
+
+  $('#inprocessParse').text(chunks.join('_'));
+}
+
+function chunkCode(code){
+  var lines = code.split('\n');
+  var linesNoComments = lines.filter((line) => {
+    return line[0] !== '*';
+  });
+  var linesReducedWhitespace = linesNoComments.map((line) => {
+    return line.replace(/\s+/g, ' ');
+  });
+  var linesRemovedInitialFinalWhitespace = linesReducedWhitespace.map((line) => {
+    return line.trim();
+  });
+
+  var linesRemovedBlanks = linesRemovedInitialFinalWhitespace.filter((line) => {
+    return line != '';
+  });
+
+  var chunksJoined = linesRemovedBlanks.join(' ');
+  return chunksJoined.split(' ');
+}
