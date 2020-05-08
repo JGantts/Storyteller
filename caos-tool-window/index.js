d4a545c873acc1fbdea7b5738b229ca4d26b9b54
Jacob Gantt <gantt42@gmail.com>
2020-05-08 03:22:36 -0400

    Proof of concept syntax highlighting.
    I need to go to be

--- caos-tool-window/index.js
+++ caos-tool-window/index.js
@@ -9,3 +9,8 @@ function injectUserCode(){
       document.getElementById('caos-result').innerHTML = result;
   });
 }
+
+function userTextChanged(){
+    document.getElementById('caos-user-code').innerHTML = '<span style="color: green;">' + $('#caos-user-code').text() + '</span>';
+    
+}
