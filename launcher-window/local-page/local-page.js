7e34cf446e940e408864c5f825e6ad4b17793451
Aiko <themaneda+github@gmail.com>
2020-05-13 17:04:10 -0400

    Added more path options

--- launcher-window/local-page/local-page.js
+++ launcher-window/local-page/local-page.js
@@ -1,12 +1,34 @@
 function launchDockingStation(){
   var { spawn } = require('child_process');
-  var executablePath = "C:/Program Files (x86)/GOG Galaxy/Games/Creatures Exodus/Docking Station/";
+  var executablePath = FindDSPath();
 
-  const engineRef = spawn(executablePath + 'engine.exe', {
-    detached: true,
-    stdio: 'ignore',
-    cwd: executablePath
-  });
+  if (executablePath) {
+  	$('#info').text(`Docking Station Path found at ${executablePath}`);
+	const engineRef = spawn(executablePath + 'engine.exe', {
+	detached: true,
+	stdio: 'ignore',
+	cwd: executablePath
+	});
 
-  engineRef.unref();
+	engineRef.unref();
+	} else {
+	$('#info').text('No Docking Station Path was found...');
+	}
+}
+
+
+function FindDSPath() {
+	const fs = require("fs")
+	const possiblePaths = [
+	"C:/Program Files (x86)/GOG Galaxy/Games/Creatures Exodus/Docking Station/",
+	"C:/Program Files (x86)/Docking Station/",
+	"C:/Program Files/Docking Station/",
+	`C:${(process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE).split('\\').join('/')}/Documents/Creatures/Docking Station`
+	]
+	for (const path in possiblePaths) {
+		if fs.existsSync(`${path}/engine.exe`) {
+			return path;
+		}
+	}
+	return false;
 }
