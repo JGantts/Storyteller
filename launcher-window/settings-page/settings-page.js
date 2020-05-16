1d483154e2f6a347bf7c390b8a8880ed75c87887
Aiko <themaneda+github@gmail.com>
2020-05-16 19:55:19 -0400

    copypasted path-based functions to settings

--- launcher-window/settings-page/settings-page.js
+++ launcher-window/settings-page/settings-page.js
@@ -1,3 +1,4 @@
+// devtools checkbox
 $('.setting-checkbox').each(function() {
   this.checked = settings.get(this.getAttribute('data-backend-name'), this.getAttribute('data-default'))
   this.addEventListener('change', (event) => {
@@ -7,6 +8,10 @@ $('.setting-checkbox').each(function() {
   setStatehint(this);
  });
 
+// gamepath display
+executablePath =  findDSPath();
+displayPathInfo();
+
 function setStatehint(checkbox){
   let stateHintDivSelector = '#' + checkbox.id + '-statehint';
   $(stateHintDivSelector).text('(' + (
@@ -14,4 +19,52 @@ function setStatehint(checkbox){
     ? $(stateHintDivSelector).attr('data-checked-statehint')
     : $(stateHintDivSelector).attr('data-unchecked-statehint')
   ) + ')');
+}
+
+function findDSPath() {
+  // checking settings first
+  if (settings.get('gamePath')) {
+    return settings.get('gamePath');
+  }
+  // then try to guess based on common paths
+  // (should maybe externalized these at some point?)
+  const possiblePaths = [
+  'C:/Program Files (x86)/GOG Galaxy/Games/Creatures Exodus/Docking Station',
+  'C:/Program Files (x86)/Docking Station',
+  'C:/Program Files/Docking Station',
+  `C:${(process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE).split('\\').join('/')}/Documents/Creatures/Docking Station`
+  ]
+  for (const path of possiblePaths) {
+    if (fs.existsSync(`${path}/engine.exe`)) {
+      settings.set('gamePath', path);
+      return path;
+    }
+  }
+  return false;
+}
+
+function displayPathInfo() {
+  if (executablePath) {
+    $('#info').text(`${executablePath}`);
+  } else {
+    $('#info').text('Your Docking Station path could not be automatically detected.');
+
+  }
+}
+
+function validateDSPath(path) {
+  if(path) {
+    const pathString = path.toString();
+    if (fs.existsSync(`${pathString}/engine.exe`)) {
+      executablePath = pathString;
+      settings.set('gamePath', pathString);
+      displayPathInfo();
+    } else {
+      $('#info').text(`Sorry, ${pathString} isn't a valid path. (Valid paths contain an engine.exe file)`);
+    }
+  }
+}
+
+function selectDSPath() {
+  validateDSPath(dialog.showOpenDialogSync({ properties: ['openDirectory', 'multiSelections'] }));
 }
