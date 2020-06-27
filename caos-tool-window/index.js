0807445930a68d4ce70dfadd5d22c9fb86af4618
Jacob Gantt <gantt42@gmail.com>
2020-06-27 13:34:18 -0400

    Split up index.js into more modules.

--- caos-tool-window/index.js
+++ caos-tool-window/index.js
@@ -6,6 +6,13 @@ const highlighter = require('./syntax-highlighting/syntax-highlighting.js')
 const { KeyCapture } = require('./key-capture.js');
 const { TreeToText } = require('./tree-to-text.js');
 const { TreeToErrors } = require('./tree-to-errors.js');
+const { CheckCode } = require('./code-editing-helper.js');
+const{
+  GetCaretPositionWithin,
+  SetCaretPositionWithin,
+  GetVisibleTextInElement,
+  GetNodesInRange,
+} = require('./html-editing-helper.js');
 const dialog = remote.dialog;
 const fs = require('fs');
 //const path = require("path");
@@ -304,8 +311,8 @@ function controlKey(event){
 }
 
 function caretKey(event){
-  let codeText = getVisibleTextInElement(codeElement);
-  let caretPositionIn = getCaretPositionWithin(codeElement);
+  let codeText = GetVisibleTextInElement(codeElement);
+  let caretPositionIn = GetCaretPositionWithin(codeElement);
   let caretPositionOut = caretPositionIn.end;
 
   switch (event.key){
@@ -334,12 +341,12 @@ function caretKey(event){
       break;
   }
 
-  setCaretPositionWithin(codeElement, caretPositionOut);
+  SetCaretPositionWithin(codeElement, caretPositionOut);
 }
 
 function editingKey(event){
-  var codeText = getVisibleTextInElement(codeElement);
-  var caretPositionIn = getCaretPositionWithin(codeElement);
+  var codeText = GetVisibleTextInElement(codeElement);
+  var caretPositionIn = GetCaretPositionWithin(codeElement);
 
   var newCodeText = '';
   var newCaretPosition = 0;
@@ -377,12 +384,12 @@ function editingKey(event){
     }
   }
 
-  checkCode(codeElement, newCodeText, caretPositionOut);
+  CheckCode(codeElement, newCodeText, caretPositionOut);
 }
 
 function insertText(text){
-  var codeText = getVisibleTextInElement(codeElement);
-  var caretPosition = getCaretPositionWithin(codeElement);
+  var codeText = GetVisibleTextInElement(codeElement);
+  var caretPosition = GetCaretPositionWithin(codeElement);
 
   let newCodeText;
   if (caretPosition.start === caretPosition.end){
@@ -397,213 +404,10 @@ function insertText(text){
       + codeText.substring(caretPosition.end, codeText.length);
   }
 
-  checkCode(codeElement, newCodeText, caretPosition.start+text.length);
+  CheckCode(codeElement, newCodeText, caretPosition.start+text.length);
 }
 
 
 function userTextKeyUp(event){
   //userTextChanged();
 }
-
-
-function checkCode(codeElement, codeText, caretPosition){
-  if (!currentFileNeedsSaving){
-    currentFileNeedsSaving = true;
-    updateTitle();
-  }
-  $('#inprocessParse').text('');
-  $('#highlighted').text('');
-
-  var whiteSpaceList =
-    codeText
-    .split('\n')
-    .map((chunk) => {
-      var astIndex = chunk.indexOf('*');
-      if (astIndex === -1){
-        return chunk;
-      }else if(astIndex === 0){
-        return '*';
-      }else{
-        return chunk.slice(0, astIndex+1);
-      }
-    })
-    .filter((chunk) => {return chunk!==null;})
-    .join('\n')
-    .match(/\s+/g);
-
-  var whiteSpaceList = whiteSpaceList ? whiteSpaceList : [];
-
-  var commentList =
-    codeText
-    .split('\n')
-    .filter((line) => {return leftTrim(line)[0]==='*'})
-    .map((line) => {return leftTrim(line)});
-
-  var codeTree = Caos(codeText);
-
-  //$('#inprocessParse').text(JSON.stringify(codeTree));
-
-  var highlighted = highlighter.highlightSyntax(codeTree, whiteSpaceList, commentList, codeText, 0);
-
-  //$('#highlighted').text(highlighted);
-
-  codeElement.innerHTML = highlighted;
-  setCaretPositionWithin(codeElement, caretPosition);
-}
-
-function leftTrim(str) {
-  return str.replace(/^\s+/g, '');
-}
-
-function getCaretPositionWithin(element) {
-    var caretPosition = 0;
-    var doc = element.ownerDocument || element.document;
-    var win = doc.defaultView || doc.parentWindow;
-    var sel;
-    sel = win.getSelection();
-    if (sel.rangeCount > 0) {
-      var range = win.getSelection().getRangeAt(0);
-
-      var preCaretRange = range.cloneRange();
-      preCaretRange.selectNodeContents(element);
-      if (range.startContainer)
-      preCaretRange.setEnd(range.startContainer, range.startOffset);
-
-      caretStartPosition = (
-        getNodesInRange(preCaretRange)
-        .filter(node =>
-          node.parentNode.className !== 'tooltip'
-          && node.nodeType === Node.TEXT_NODE
-        )
-        .reduce(
-          (total, node) => total + node.textContent.length,
-          0
-        )
-        - (preCaretRange.endContainer.textContent.length - preCaretRange.endOffset));
-
-      var prePlusInCaretRange = range.cloneRange();
-      prePlusInCaretRange.selectNodeContents(element);
-      prePlusInCaretRange.setEnd(range.endContainer, range.endOffset);
-
-      caretEndPosition = (
-        getNodesInRange(prePlusInCaretRange)
-        .filter(node =>
-          node.parentNode.className !== 'tooltip'
-          && node.nodeType === Node.TEXT_NODE
-        )
-        .reduce(
-          (total, node) => total + node.textContent.length,
-          0
-        )
-        - (prePlusInCaretRange.endContainer.textContent.length - prePlusInCaretRange.endOffset));
-    }
-    return {start: caretStartPosition, end: caretEndPosition};
-}
-
-function setCaretPositionWithin(element, caretPosition) {
-  var doc = element.ownerDocument || element.document;
-  var win = doc.defaultView || doc.parentWindow;
-  var sel;
-  sel = win.getSelection();
-  let range = doc.createRange();
-  range.selectNode(element);
-  let visibleTextNodes =
-    getNodesInRange(range)
-    .filter(node =>
-      node.parentNode.tagName !== 'DIV'
-    )
-    .filter(node =>
-      node.parentNode.className !== 'tooltip'
-    )
-    .filter(node =>
-      node.nodeType === Node.TEXT_NODE
-    );
-
-  let textLength = visibleTextNodes.reduce((total, node) => total + node.textContent.length, 0);
-  if (caretPosition > textLength){
-    caretPosition = textLength;
-  }else if (caretPosition < 0){
-    caretPosition = 0;
-  }
-
-  if (visibleTextNodes.length > 0){
-    var currentTextLength = 0
-    var index = 0;
-    var done = false;
-    do{
-      currentTextLength += visibleTextNodes[index].textContent.length;
-      if (currentTextLength >= caretPosition){
-        done = true;
-      }
-      index++;
-    }while(index < visibleTextNodes.length && !done)
-
-    let offsetInto = visibleTextNodes[index-1].length - (currentTextLength - caretPosition);
-
-    range.setStart(visibleTextNodes[index-1], offsetInto);
-    range.setEnd(visibleTextNodes[index-1], offsetInto);
-    sel.removeAllRanges();
-    sel.addRange(range);
-  }else{
-    //Don't do anything?
-  }
-}
-
-function getVisibleTextInElement(element){
-  var doc = element.ownerDocument || element.document;
-
-  let range = doc.createRange();
-  range.selectNode(element);
-  return (getNodesInRange(range)
-    .filter(node =>
-      {
-        return node.parentNode.className !== 'tooltip'
-          && node.parentNode.className.includes('syntax-')
-          && node.nodeType === Node.TEXT_NODE;
-      }
-    )
-    .reduce(
-      (total, node) => {
-        return total + node.textContent;
-      },
-      ''
-    )
-    /*.replace(/&lt;/g,'<')
-    .replace(/&gt;/g,'>')
-    .replace(/&amp;/g,'&')*/);
-}
-
-function getNodesInRange(range){
-  var startNode = range.startContainer.childNodes[range.startOffset]
-    || range.startContainer;//it's a text node
-  var endNode = range.endContainer.childNodes[range.endOffset]
-    || range.endContainer;
-
-  if (startNode == endNode && startNode.childNodes.length === 0) {
-    return [startNode];
-  };
-
-  var nodes = [];
-  do {
-    nodes.push(startNode);
-  }
-  while ((startNode = getNextNode(startNode, false, endNode))
-    && (startNode != endNode));
-  nodes.push(endNode);
-  return nodes;
-};
-
-function getNextNode(node, skipChildren, endNode){
-  //if there are child nodes and we didn't come from a child node
-  if (endNode == node) {
-    return null;
-  }
-  if (node.firstChild && !skipChildren) {
-    return node.firstChild;
-  }
-  if (!node.parentNode){
-    return null;
-  }
-  return node.nextSibling
-         || getNextNode(node.parentNode, true, endNode);
-};
