003de686e386a5cd078c53963d20a736a2fd4533
Jacob Gantt <gantt42@gmail.com>
2020-07-17 20:08:53 -0400

    Update index.js

--- breed-sprite-kit-window/index.js
+++ breed-sprite-kit-window/index.js
@@ -12,35 +12,35 @@ tabGroup.addTab({
 tabGroup.addTab({
   title: '',
   iconURL: 'icons/png-crop.svg',
-  src: 'http://google.com',
+  src: 'png-crop-tab/index.html',
   visible: true,
   closable: false,
 });
 tabGroup.addTab({
   title: '',
   iconURL: 'icons/png-to-c16.svg',
-  src: 'http://electron.atom.io',
+  src: 'png-to-c16-tab/index.html',
   visible: true,
   closable: false,
 });
 tabGroup.addTab({
   title: '',
   iconURL: 'icons/reslotter.svg',
-  src: 'http://electron.atom.io',
+  src: 'reslotter-tab/index.html',
   visible: true,
   closable: false,
 });
 tabGroup.addTab({
   title: '',
   iconURL: 'icons/att-editor.svg',
-  src: 'http://electron.atom.io',
+  src: 'att-editor-tab/index.html',
   visible: true,
   closable: false,
 });
 tabGroup.addTab({
   title: '',
   iconURL: 'icons/apperance-genetics-editor.svg',
-  src: 'http://electron.atom.io',
+  src: 'apperance-genetics-editor-tab/index.html',
   visible: true,
   closable: false,
 });
