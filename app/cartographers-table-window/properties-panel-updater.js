1254400a0f33658bbf7217f548e925cad94d8eb5
Jacob Gantt <gantt42@gmail.com>
2021-11-26 15:16:17 -0500

    CA rates by room type

--- app/cartographers-table-window/properties-panel-updater.js
+++ app/cartographers-table-window/properties-panel-updater.js
@@ -138,6 +138,742 @@ function updatePropertiesPanel(panel, selection, dataStructures) {
 
 let = roomtypeTemplates = null;
 
+let roomtypeNames = [
+  "Atmosphere",
+  "Wooden Walkway",
+  "Concrete Walkway",
+  "Indoor Corridor",
+  "Outdoor Corridor",
+  "Normal Soil",
+  "Boggy Soil",
+  "Drained Soil",
+  "Fresh Water",
+  "Salt Water",
+  "Ettin",
+];
+
+let roomtypeCa0GainRates = [
+  1.000000,
+  0.000000,
+  1.000000,
+  0.000000,
+  0.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+  0.000000,
+  0.000000,
+];
+let roomtypeCa0LossRates = [
+  0.000000,
+  0.000000,
+  0.000000,
+  0.000000,
+  0.000000,
+  0.000000,
+  0.000000,
+  0.000000,
+  0.000000,
+  0.000000,
+  0.000000,
+];
+let roomtypeCa0DiffRates = [
+  1.000000,
+  0.000000,
+  1.000000,
+  0.000000,
+  0.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+  0.000000,
+  0.000000,
+]
+
+let roomtypeCa1GainRates = [
+  1.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+];
+let roomtypeCa1LossRates = [
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+];
+let roomtypeCa1DiffRates = [
+  1.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+];
+
+let roomtypeCa2GainRates = [
+  0.800000,
+  0.600000,
+  0.600000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.000000,
+  0.000000,
+  0.800000,
+];
+let roomtypeCa2LossRates = [
+  0.010000,
+  0.001000,
+  0.001000,
+  0.010000,
+  0.010000,
+  0.010000,
+  0.010000,
+  0.010000,
+  0.000000,
+  0.000000,
+  0.010000,
+];
+let roomtypeCa2DiffRates = [
+  0.700000,
+  0.700000,
+  0.700000,
+  0.700000,
+  0.700000,
+  0.700000,
+  0.700000,
+  0.700000,
+  0.000000,
+  0.000000,
+  0.700000,
+];
+
+let roomtypeCa3GainRates = [
+  0.900000,
+  0.300000,
+  0.300000,
+  0.300000,
+  0.300000,
+  1.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+  0.300000,
+];
+let roomtypeCa3LossRates = [
+  0.050000,
+  0.900000,
+  0.900000,
+  0.900000,
+  0.900000,
+  0.005000,
+  0.001000,
+  0.010000,
+  0.000100,
+  0.000100,
+  0.900000,
+];
+let roomtypeCa3DiffRates = [
+  1.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+];
+
+let roomtypeCa4GainRates = [
+  0.000000,
+  0.000000,
+  0.000000,
+  0.000000,
+  0.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+  0.000000,
+  0.000000,
+  0.000000,
+];
+let roomtypeCa4LossRates = [
+  0.000000,
+  0.000000,
+  0.000000,
+  0.000000,
+  0.000000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.000000,
+  0.000000,
+  0.000000,
+];
+let roomtypeCa4DiffRates = [
+  0.000000,
+  0.000000,
+  0.000000,
+  0.000000,
+  0.000000,
+  0.900000,
+  0.900000,
+  0.900000,
+  0.000000,
+  0.000000,
+  0.000000,
+];
+
+let roomtypeCa5GainRates = [
+  1.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+  0.900000,
+  0.900000,
+  1.000000,
+];
+let roomtypeCa5LossRates = [
+  0.100000,
+  0.500000,
+  0.500000,
+  0.500000,
+  0.500000,
+  0.500000,
+  0.500000,
+  0.500000,
+  0.001000,
+  0.001000,
+  0.500000,
+];
+let roomtypeCa5DiffRates = [
+  0.100000,
+  1.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+  1.000000,
+];
+
+let roomtypeCa6GainRates = [
+  0.990000,
+  0.990000,
+  0.990000,
+  0.990000,
+  0.990000,
+  0.400000,
+  0.400000,
+  0.400000,
+  0.990000,
+  0.990000,
+  0.990000,
+];
+let roomtypeCa6LossRates = [
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+];
+let roomtypeCa6DiffRates = [
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+];
+
+let roomtypeCa7GainRates = [
+  0.990000,
+  0.990000,
+  0.990000,
+  0.990000,
+  0.990000,
+  0.400000,
+  0.400000,
+  0.400000,
+  0.990000,
+  0.990000,
+  0.990000,
+];
+let roomtypeCa7LossRates = [
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+];
+let roomtypeCa7DiffRates = [
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+];
+
+let roomtypeCa8GainRates = [
+  0.990000,
+  0.990000,
+  0.990000,
+  0.990000,
+  0.990000,
+  0.400000,
+  0.400000,
+  0.400000,
+  0.990000,
+  0.990000,
+  0.990000,
+];
+let roomtypeCa8LossRates = [
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+];
+let roomtypeCa8DiffRates = [
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+];
+
+let roomtypeCa10GainRates = [
+  0.990000,
+  0.990000,
+  0.990000,
+  0.990000,
+  0.990000,
+  0.400000,
+  0.400000,
+  0.400000,
+  0.990000,
+  0.990000,
+  0.990000,
+];
+let roomtypeCa10LossRates = [
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+];
+let roomtypeCa10DiffRates = [
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+];
+
+let roomtypeCa11GainRates = [
+  0.990000,
+  0.990000,
+  0.990000,
+  0.990000,
+  0.990000,
+  0.400000,
+  0.400000,
+  0.400000,
+  0.990000,
+  0.990000,
+  0.990000,
+];
+let roomtypeCa11LossRates = [
+  0.990000,
+  0.990000,
+  0.990000,
+  0.990000,
+  0.990000,
+  0.400000,
+  0.400000,
+  0.400000,
+  0.990000,
+  0.990000,
+  0.990000,
+];
+let roomtypeCa11DiffRates = [
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+];
+
+let roomtypeCa12GainRates = [
+  0.990000,
+  0.990000,
+  0.990000,
+  0.990000,
+  0.990000,
+  0.400000,
+  0.400000,
+  0.400000,
+  0.990000,
+  0.990000,
+  0.990000,
+];
+let roomtypeCa12LossRates = [
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+];
+let roomtypeCa12DiffRates = [
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+];
+
+let roomtypeCa13GainRates = [
+  0.990000,
+  0.990000,
+  0.990000,
+  0.990000,
+  0.990000,
+  0.400000,
+  0.400000,
+  0.400000,
+  0.990000,
+  0.990000,
+  0.990000,
+];
+let roomtypeCa13LossRates = [
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+];
+let roomtypeCa13DiffRates = [
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+];
+
+let roomtypeCa14GainRates = [
+  0.990000,
+  0.990000,
+  0.990000,
+  0.990000,
+  0.990000,
+  0.400000,
+  0.400000,
+  0.400000,
+  0.990000,
+  0.990000,
+  0.990000,
+];
+let roomtypeCa14LossRates = [
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+];
+let roomtypeCa14DiffRates = [
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+];
+
+let roomtypeCa15GainRates = [
+  0.990000,
+  0.990000,
+  0.990000,
+  0.990000,
+  0.990000,
+  0.400000,
+  0.400000,
+  0.400000,
+  0.990000,
+  0.990000,
+  0.990000,
+];
+let roomtypeCa15LossRates = [
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+];
+let roomtypeCa15DiffRates = [
+  0.950000,
+  0.950000,
+  0.950000,
+  0.950000,
+  0.950000,
+  0.950000,
+  0.950000,
+  0.950000,
+  0.950000,
+  0.950000,
+  0.950000,
+];
+
+let roomtypeCa16GainRates = [
+  0.990000,
+  0.990000,
+  0.990000,
+  0.990000,
+  0.990000,
+  0.400000,
+  0.400000,
+  0.400000,
+  0.990000,
+  0.990000,
+  0.990000,
+];
+let roomtypeCa16LossRates = [
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+];
+let roomtypeCa16DiffRates = [
+  0.950000,
+  0.950000,
+  0.950000,
+  0.950000,
+  0.950000,
+  0.950000,
+  0.950000,
+  0.950000,
+  0.950000,
+  0.950000,
+  0.950000,
+];
+
+let roomtypeCa17GainRates = [
+  0.950000,
+  0.950000,
+  0.950000,
+  0.950000,
+  0.950000,
+  0.950000,
+  0.950000,
+  0.950000,
+  0.950000,
+  0.950000,
+  0.950000,
+];
+let roomtypeCa17LossRates = [
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+];
+let roomtypeCa17DiffRates = [
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+];
+
+let roomtypeCa18GainRates = [
+  0.990000,
+  0.990000,
+  0.990000,
+  0.990000,
+  0.990000,
+  0.400000,
+  0.400000,
+  0.400000,
+  0.990000,
+  0.990000,
+  0.000000,
+];
+let roomtypeCa18LossRates = [
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+  0.001000,
+];
+let roomtypeCa18DiffRates = [
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+  0.800000,
+];
+
+
+
 function updateRoomtypePanel(panel, selection, dataStructures) {
   if (!roomtypeTemplates) {
       roomtypeTemplates = {
@@ -176,61 +912,80 @@ function updateRoomtypePanel(panel, selection, dataStructures) {
 
       }
 
-      let roomTypeName = "";
-      switch (roomtype) {
-        case -1:
-          roomTypeName = "Please add a type to this room.";
-          break;
+      currentClone.querySelector("#title").innerHTML = roomtypeNames[roomtype];
 
-        case 0:
-          roomTypeName = "Atmosphere";
-          break;
+      currentClone.querySelector("#ca-0-gain").innerHTML = roomtypeCa0GainRates[roomtype];
+      currentClone.querySelector("#ca-0-loss").innerHTML = roomtypeCa0LossRates[roomtype];
+      currentClone.querySelector("#ca-0-diff").innerHTML = roomtypeCa0DiffRates[roomtype];
 
-        case 1:
-          roomTypeName = "Wooden Walkway";
-          break;
+      currentClone.querySelector("#ca-1-gain").innerHTML = roomtypeCa1GainRates[roomtype];
+      currentClone.querySelector("#ca-1-loss").innerHTML = roomtypeCa1LossRates[roomtype];
+      currentClone.querySelector("#ca-1-diff").innerHTML = roomtypeCa1DiffRates[roomtype];
 
-        case 2:
-          roomTypeName = "Concrete Walkway";
-          break;
+      currentClone.querySelector("#ca-2-gain").innerHTML = roomtypeCa2GainRates[roomtype];
+      currentClone.querySelector("#ca-2-loss").innerHTML = roomtypeCa2LossRates[roomtype];
+      currentClone.querySelector("#ca-2-diff").innerHTML = roomtypeCa2DiffRates[roomtype];
 
-        case 3:
-          roomTypeName = "Indoor Corridor";
-          break;
+      currentClone.querySelector("#ca-3-gain").innerHTML = roomtypeCa3GainRates[roomtype];
+      currentClone.querySelector("#ca-3-loss").innerHTML = roomtypeCa3LossRates[roomtype];
+      currentClone.querySelector("#ca-3-diff").innerHTML = roomtypeCa3DiffRates[roomtype];
 
-        case 4:
-          roomTypeName = "Outdoor Corridor";
-          break;
+      currentClone.querySelector("#ca-4-gain").innerHTML = roomtypeCa4GainRates[roomtype];
+      currentClone.querySelector("#ca-4-loss").innerHTML = roomtypeCa4LossRates[roomtype];
+      currentClone.querySelector("#ca-4-diff").innerHTML = roomtypeCa4DiffRates[roomtype];
 
-        case 5:
-          roomTypeName = "Normal Soil";
-          break;
+      currentClone.querySelector("#ca-5-gain").innerHTML = roomtypeCa5GainRates[roomtype];
+      currentClone.querySelector("#ca-5-loss").innerHTML = roomtypeCa5LossRates[roomtype];
+      currentClone.querySelector("#ca-5-diff").innerHTML = roomtypeCa5DiffRates[roomtype];
 
-        case 6:
-          roomTypeName = "Boggy Soil";
-          break;
+      currentClone.querySelector("#ca-6-gain").innerHTML = roomtypeCa6GainRates[roomtype];
+      currentClone.querySelector("#ca-6-loss").innerHTML = roomtypeCa6LossRates[roomtype];
+      currentClone.querySelector("#ca-6-diff").innerHTML = roomtypeCa6DiffRates[roomtype];
 
-        case 7:
-          roomTypeName = "Drained Soil";
-          break;
+      currentClone.querySelector("#ca-7-gain").innerHTML = roomtypeCa7GainRates[roomtype];
+      currentClone.querySelector("#ca-7-loss").innerHTML = roomtypeCa7LossRates[roomtype];
+      currentClone.querySelector("#ca-7-diff").innerHTML = roomtypeCa7DiffRates[roomtype];
 
-        case 8:
-          roomTypeName = "Fresh Water";
-          break;
+      currentClone.querySelector("#ca-8-gain").innerHTML = roomtypeCa8GainRates[roomtype];
+      currentClone.querySelector("#ca-8-loss").innerHTML = roomtypeCa8LossRates[roomtype];
+      currentClone.querySelector("#ca-8-diff").innerHTML = roomtypeCa8DiffRates[roomtype];
 
-        case 9:
-          roomTypeName = "Salt Water";
-          break;
+      currentClone.querySelector("#ca-10-gain").innerHTML = roomtypeCa10GainRates[roomtype];
+      currentClone.querySelector("#ca-10-loss").innerHTML = roomtypeCa10LossRates[roomtype];
+      currentClone.querySelector("#ca-10-diff").innerHTML = roomtypeCa10DiffRates[roomtype];
 
-        case 10:
-          roomTypeName = "Ettin Home";
-          break;
+      currentClone.querySelector("#ca-11-gain").innerHTML = roomtypeCa11GainRates[roomtype];
+      currentClone.querySelector("#ca-11-loss").innerHTML = roomtypeCa11LossRates[roomtype];
+      currentClone.querySelector("#ca-11-diff").innerHTML = roomtypeCa11DiffRates[roomtype];
 
-        default:
-          console.log(roomtype)
-          assert(false, "wut?");
-      }
-      currentClone.querySelector("#title").innerHTML = roomTypeName;
+      currentClone.querySelector("#ca-12-gain").innerHTML = roomtypeCa12GainRates[roomtype];
+      currentClone.querySelector("#ca-12-loss").innerHTML = roomtypeCa12LossRates[roomtype];
+      currentClone.querySelector("#ca-12-diff").innerHTML = roomtypeCa12DiffRates[roomtype];
+
+      currentClone.querySelector("#ca-13-gain").innerHTML = roomtypeCa13GainRates[roomtype];
+      currentClone.querySelector("#ca-13-loss").innerHTML = roomtypeCa13LossRates[roomtype];
+      currentClone.querySelector("#ca-13-diff").innerHTML = roomtypeCa13DiffRates[roomtype];
+
+      currentClone.querySelector("#ca-14-gain").innerHTML = roomtypeCa14GainRates[roomtype];
+      currentClone.querySelector("#ca-14-loss").innerHTML = roomtypeCa14LossRates[roomtype];
+      currentClone.querySelector("#ca-14-diff").innerHTML = roomtypeCa14DiffRates[roomtype];
+
+      currentClone.querySelector("#ca-15-gain").innerHTML = roomtypeCa15GainRates[roomtype];
+      currentClone.querySelector("#ca-15-loss").innerHTML = roomtypeCa15LossRates[roomtype];
+      currentClone.querySelector("#ca-15-diff").innerHTML = roomtypeCa15DiffRates[roomtype];
+
+      currentClone.querySelector("#ca-16-gain").innerHTML = roomtypeCa16GainRates[roomtype];
+      currentClone.querySelector("#ca-16-loss").innerHTML = roomtypeCa16LossRates[roomtype];
+      currentClone.querySelector("#ca-16-diff").innerHTML = roomtypeCa16DiffRates[roomtype];
+
+      currentClone.querySelector("#ca-17-gain").innerHTML = roomtypeCa17GainRates[roomtype];
+      currentClone.querySelector("#ca-17-loss").innerHTML = roomtypeCa17LossRates[roomtype];
+      currentClone.querySelector("#ca-17-diff").innerHTML = roomtypeCa17DiffRates[roomtype];
+
+      currentClone.querySelector("#ca-18-gain").innerHTML = roomtypeCa18GainRates[roomtype];
+      currentClone.querySelector("#ca-18-loss").innerHTML = roomtypeCa18LossRates[roomtype];
+      currentClone.querySelector("#ca-18-diff").innerHTML = roomtypeCa18DiffRates[roomtype];
+
     }
 }
 
