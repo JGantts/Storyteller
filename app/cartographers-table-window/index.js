9a186a190af94d242dd816d3b4fda2b06ab85e1f
Jacob Gantt <gantt42@gmail.com>
2021-12-20 11:08:03 -0500

    Add fps logging

--- app/cartographers-table-window/index.js
+++ app/cartographers-table-window/index.js
@@ -1411,7 +1411,7 @@ async function redrawSelection(timestamp) {
           oldTimestamp = timestamp;
           fps = Math.round(1 / secondsPassed);
       }
-      //console.log(fps)
+      console.log(fps)
 
 
 
