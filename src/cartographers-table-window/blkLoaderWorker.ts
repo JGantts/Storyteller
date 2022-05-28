e5459f6b55a1adedfb355e5f3b89737960cc6165
daniel <dsbadal@gmail.com>
2022-05-28 09:56:52 -0700

    Allow status updates on BLK loading

--- src/cartographers-table-window/blkLoaderWorker.ts
+++ src/cartographers-table-window/blkLoaderWorker.ts
@@ -5,15 +5,19 @@ const { loadBackground:mLoad } = require("./../hard-dependencies/NodeBLKLoader.j
 
 onmessage = async (e) => {
     const absolutePath = e.data.absolutePath;
-    const bytes = await mLoad(absolutePath, (pass: number, index: number, total: number) => {
-        postMessage({
-            progress: {
-                pass,
-                index,
-                total
-            }
-        });
-    });
+    let logger: Nullable<(pass: number, index: number, total: number)=>void> = null;
+    if (e.data.withStatus) {
+        logger = (pass: number, index: number, total: number) => {
+            postMessage({
+                progress: {
+                    pass,
+                    index,
+                    total
+                }
+            });
+        };
+    }
+    const bytes = await mLoad(absolutePath, logger);
     console.log("STITCHED BLK!!");
     postMessage({ result: bytes });
 }
--- src/cartographers-table-window/loadBackground.ts
+++ src/cartographers-table-window/loadBackground.ts
@@ -1,48 +1,75 @@
 // @ts-ignore
-const {loadBackground:mLoad} = require("./../hard-dependencies/NodeBLKLoader.js");
+const {loadBackground: mLoad} = require("./../hard-dependencies/NodeBLKLoader.js");
+const {setStatusRight, clearStatusRight} = require('./statusFooter.js');
 
-function loadBackground(absolutePath: string, callback: (image: ImageData | HTMLImageElement) => void) {
+function loadBackground(absolutePath: string, withStatus: boolean, callback: (image: ImageData | HTMLImageElement) => void) {
     if (window.Worker) {
-        console.log("Using worker");
-        workerPromise(absolutePath, callback);
+        console.log("Loading BLK with worker");
+        workerPromise(absolutePath, withStatus, callback);
         return;
     }
-    console.log("Not using worker");
-    const logger = (pass: number, index: number, total: number) => {
-        const percent = (((index + ((pass - 1) * total)) / (total * 2.0)) * 100).toFixed(0);
-        console.log("ParsingBLK: " + percent + "%");
+    // Get optional logger
+    const logger = withStatus ? getPercentLogger() : null;
+    
+    // Load BLK
+    console.log("Loading BLK in window");
+    mLoad(absolutePath, logger)
+        .then((bytes: Int8Array) => {
+            clearStatusRight();
+            pushImageFromBytes(bytes, callback);
+        });
+}
+
+function getPercentLogger():(pass: number, index: number, total: number) => void {
+    let lastPercent = "";
+    return (pass: number, index: number, total: number) => {
+        const percent = ((index * 100.0) / total).toFixed(0);
+        if (percent == lastPercent) {
+            return;
+        }
+        lastPercent = percent;
+        let action: string;
+        if (pass == 1) {
+            action = "Parsing"
+        } else {
+            action = "Stitching";
+        }
+        setStatusRight(`${action} Background: ${percent}%"`);
     };
-    mLoad(absolutePath, logger).then((bytes: Int8Array) => {
-        pushImageFromBytes(bytes, callback);
-    });
 }
 
 
-function workerPromise(absolutePath: string, callback: (image: ImageData | HTMLImageElement) => void) {
+function workerPromise(absolutePath: string, withStatus: boolean, callback: (image: ImageData | HTMLImageElement) => void) {
     const worker = new Worker("./blkLoaderWorker.js");
     const terminate = () => {
         worker.onerror = null;
         worker.onmessage = null;
         worker.terminate();
     }
+    const logger = withStatus ? getPercentLogger() : null;
     worker.onerror = (e) => {
         terminate();
         throw Error("Failed to parse BLK: " + e.message);
     }
+    const start = Date.now();
     worker.onmessage = (e) => {
         if (e.data == null) {
             terminate();
             throw Error("Failed to parse BLK");
         } else if (e.data.result) {
+            const time = (Date.now() - start) / 1000;
+            console.log(`Took: ${time} seconds to parse BLK`);
             pushImageFromBytes(e.data.result as Int8Array, callback)
+            clearStatusRight();
             terminate()
         } else {
             const {pass, index, total} = e.data.progress;
-            const percent = (((index + ((pass - 1) * total)) / (total * 2.0)) * 100).toFixed(0);
-            console.log("ParsingBLK: " + percent + "%");
+            if (withStatus) {
+                logger!!(pass, index, total);
+            }
         }
     }
-    worker.postMessage({absolutePath})
+    worker.postMessage({absolutePath, withStatus})
 }
 
 function pushImageFromBytes(bytes: Int8Array, callback: (image: ImageData | HTMLImageElement) => void) {
