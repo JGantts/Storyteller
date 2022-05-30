1bafb43ccb3b21ebcc4ddd5bc5094dc9235a5b22
daniel <dsbadal@gmail.com>
2022-05-30 11:17:03 -0700

    Add missing line permeability commands
    Command updates and additions were missing from previous pull request.

--- src/cartographers-table-window/commonFunctions.ts
+++ src/cartographers-table-window/commonFunctions.ts
@@ -8,8 +8,62 @@ function getSortedId(idA: string, idB: string) {
     }
 }
 
+export function isBetween(a:number, min: number, max: number, inclusive: boolean = false) {
+    if (inclusive) {
+        return a >= min && a <= max;
+    } else {
+        return a > min && a < max;
+    }
+}
+
+function hslToRgb(hue: number, saturation: number, luminosity: number){
+    let red, green, blue;
+    
+    if (saturation == 0) {
+        red = green = blue = luminosity; // achromatic
+    } else {
+        let hue2rgb = (p: number, q: number, t: number) => {
+            if(t < 0) t += 1;
+            if(t > 1) t -= 1;
+            if(t < 1/6) return p + (q - p) * 6 * t;
+            if(t < 1/2) return q;
+            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+            return p;
+        }
+        
+        let q = luminosity < 0.5 ? luminosity * (1 + saturation) : luminosity + saturation - luminosity * saturation;
+        let p = 2 * luminosity - q;
+        red = hue2rgb(p, q, hue + 1/3);
+        green = hue2rgb(p, q, hue);
+        blue = hue2rgb(p, q, hue - 1/3);
+    }
+    
+    return [Math.round(red * 255), Math.round(green * 255), Math.round(blue * 255)];
+}
+
+function getDoorPermeabilityColor(permeability: number, maxHue = 120, minHue = 0): string {
+    // if (permeability < 0) {
+    //     return 'rgb(005, 170, 255)';
+    // } else if (permeability === 0) {
+    //     return 'rgb(228, 000, 107)';
+    // } else if (permeability < 100) {
+    //     return 'rgb(207, 140, 003)';
+    // } else if (permeability === 100) {
+    //     return 'rgb(172, 255, 083)';
+    // }
+    if (permeability < 0) {
+        return 'rgb(005, 170, 255)';
+    }
+    const hue = (permeability / 100) * (maxHue - minHue) + minHue;
+    // const [red, green, blue] = hslToRgb(hue, 100, 50);
+    // return `rgb(${red}, ${green}, ${blue})`;
+    return `hsl(${hue}, 100%, 50%)`;
+}
+
 module.exports = {
     common: {
         getSortedId,
     },
+    getDoorPermeabilityColor,
+    isBetween
 }
