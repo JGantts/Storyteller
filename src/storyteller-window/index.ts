85ae128f886bc4a7a806dd376f2c203b247c9faa
Jacob Gantt <gantt42@gmail.com>
2022-06-23 19:47:08 -0400

    whitespace

--- src/storyteller-window/index.ts
+++ src/storyteller-window/index.ts
@@ -11,17 +11,13 @@ export function initPage () {
 
   let settingsTab = document.getElementById("nav-btn-settings") as EventTarget&Element;
   settingsTab.addEventListener("click", (e: Event) => MenuBar.loadSettingsPage(settingsTab.id));
-  let localTab = document.getElementById("nav-btn-local") as  EventTarget&Element;
-  //if (!(localTab instanceof EventTarget&Element)) { throw new Error(`ID: 5682068764 Reference Data: ${JSON.stringify(localTab)}`); }
+  let localTab = document.getElementById("nav-btn-local") as EventTarget&Element;
   localTab?.addEventListener("click", (e: Event) => MenuBar.loadLocalPage(localTab?.id));
-  let serverTab = document.getElementById("nav-btn-server") as  EventTarget&Element;
-  //if (!(serverTab instanceof EventTarget&Element)) { throw new Error(`ID: 7441965918 Reference Data: ${JSON.stringify(serverTab)}`); }
+  let serverTab = document.getElementById("nav-btn-server") as EventTarget&Element;
   serverTab?.addEventListener("click", (e: Event) => MenuBar.loadServerPage(serverTab?.id));
-  let eemfooTab = document.getElementById("nav-btn-eemfoo") as  EventTarget&Element;
-  //if (!(eemfooTab instanceof EventTarget&Element)) { throw new Error(`ID: 5475770654 Reference Data: ${JSON.stringify(eemfooTab)}`); }
+  let eemfooTab = document.getElementById("nav-btn-eemfoo") as EventTarget&Element;
   eemfooTab?.addEventListener("click", (e: Event) => MenuBar.loadEemFooPage(eemfooTab?.id));
-  let devTab = document.getElementById("nav-btn-dev") as  EventTarget&Element;
-  //if (!(devTab instanceof EventTarget&Element)) { throw new Error(`ID: 4389403662 Reference Data: ${JSON.stringify(devTab)}`); }
+  let devTab = document.getElementById("nav-btn-dev") as EventTarget&Element;
   devTab?.addEventListener("click", (e: Event) => MenuBar.loadDevToolsPage(devTab?.id));
 }
 
